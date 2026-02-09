699d5960e13b383ce3a3728ea4f312c8066ca818
vasiaw <fvlog36@gmail.com>
2026-02-09 16:47:36 +0200

    LAB2_C#

---
+++ LAB2_C#/LAB2_C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using LAB2_C_;
+
+String_Class word1 = new String_Class();
+String_Class word2 = new String_Class("cup");
+String_Class word3 = new String_Class(word2);
+
+Console.Write("Enter word1: ");
+word1.Set_Word(Console.ReadLine());
+
+Console.Write("The lentgh of word1 is: " + word1.Get_Length());
+Console.WriteLine();
+
+Console.Write("The lentgh of word2 and word3 is: "+ word2.Get_Length());
+Console.WriteLine();
+
+word1.Sort_Descending();
+Console.Write("Sorted word1: " + word1.Get_Word());
+
+
+Console.WriteLine();
+
+word2.Sort_Descending();
+Console.Write("Sorted word2 and word3: " + word2.Get_Word());
+
+
---
+++ LAB2_C#/LAB2_C#/String_Class.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB2_C_
+{
+    internal class String_Class
+    {
+        private string value;
+
+        public String_Class() //Конструктор за замовчуванням
+        {
+            value = " ";
+
+        }
+
+        public String_Class(string str) //Конструктор с параметром 
+        {
+            str = "Aviation";
+            value = str;
+        }
+        
+        public String_Class(String_Class other_value) //Конструктор копіювання
+        {
+            value = other_value.value;
+       }
+
+        public void Set_Word(string str)
+        {
+            value = str;
+        }
+        public string Get_Word()
+        {
+            return value;
+        }
+
+        public int Get_Length()
+        {
+            return value.Length;
+        }
+
+        public void Sort_Descending()
+        {
+            char[] arr = value.ToCharArray();
+            Array.Sort(arr);
+            Array.Reverse(arr);
+            value = new string(arr);
+        }
+        ~String_Class()
+        {
+            
+        }
+
+    }
+}
